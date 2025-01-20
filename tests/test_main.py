3f866180482a7c3fae4a071bb45c42ac1d832ff8
maukan <maukanm22@gmail.com>
2025-01-20 13:44:56 -0500

    added new notes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
 def test_dataframe_operations(df):
-    
-    assert df.shape == (954, 29)
-    assert df.isna().mean().mean() < 0.5
+  assert df.shape == (954, 29)
+  assert df.isna().mean().mean() < 0.5
